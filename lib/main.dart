d6c2c0fdbb949fc123a99266dee15b2c2c28f8a0
almosrof120 <mosrof35-645@diu.edu.bd>
2024-03-10 17:14:49 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+int x = 0;
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    print('rebuild');
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Center(child: Text('Lottery App')),
+        ),
+        body: SafeArea(
+            child: Center(
+          child: Text(
+            x.toString(),
+            style: const TextStyle(
+              fontSize: 50,
+            ),
+          ),
+        )),
+        floatingActionButton: FloatingActionButton(onPressed: (() {
+          x++;
+          setState(() {});
+          print(x.toString());
+        })),
+      ),
+    );
+  }
+}
